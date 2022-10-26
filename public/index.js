9cff7371efc923a2b13a2854108c444591bf0a98
kyungmin123 <kimkm3972@naver.com>
2022-10-26 19:43:22 +0900

    ts 변환

--- public/index.js
+++ public/index.js
@@ -1,75 +1,70 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const divEl = document.createElement("div");
-
-document.querySelector("#btn").onclick = async () => {
-    const response1 = await fetch("/api/account/income", {
+document.querySelector("#btn").onclick = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response1 = yield fetch("/api/account/income", {
         method: "GET",
     });
-    const response2 = await fetch("/api/account/expend", {
+    const response2 = yield fetch("/api/account/expend", {
         method: "GET",
     });
-    
-    const data1 = await response1.json();
-    const data2 = await response2.json();
-    
+    const data1 = yield response1.json();
+    const data2 = yield response2.json();
     console.log(data1);
     console.log(data2);
-    
-}
-
-document.querySelector("#btn2").onclick = async () => {
-    const response3 = await fetch("/api/account/expend/id/1", {
+});
+document.querySelector("#btn2").onclick = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response3 = yield fetch("/api/account/expend/id/1", {
         method: "GET",
     });
-
-    const data3 = await response3.json();
+    const data3 = yield response3.json();
     console.log(data3);
-
-};
-
-(document.querySelector("#btn5")).onclick = async () => {
-    const response3 = await fetch("/api/account/income/id/1", {
+});
+document.querySelector("#btn5").onclick = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response3 = yield fetch("/api/account/income/id/1", {
         method: "GET",
     });
-
-    const data3 = await response3.json();
+    const data3 = yield response3.json();
     console.log(data3);
-
-}
-
-(document.querySelector("#btn3")).onclick = async () => {
-    const response = await fetch("/api/account/income/payDay/25", {
+});
+document.querySelector("#btn3").onclick = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("/api/account/income/payDay/25", {
         method: "GET",
     });
-
-    const data = await response.json();
+    const data = yield response.json();
     console.log(data);
-}
-
-(document.querySelector("#btn4")).onclick = async () => {
-    const response = await fetch("/api/account/expend/payDay/25", {
+});
+document.querySelector("#btn4").onclick = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("/api/account/expend/payDay/25", {
         method: "GET",
     });
-
-    const data = await response.json();
+    const data = yield response.json();
     console.log(data);
-}
-
-(document.querySelector("#btnPut")).onclick = async () => {
-    const response = await fetch("/api/account/expend/id/1", {
+});
+document.querySelector("#btnPut").onclick = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("/api/account/expend/id/1", {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({ content: {
-                                    payedMoney: 20000000, 
-                                    payment: "카드",
-                                    category: "식비", 
-                                    memo: "안녕하세요", 
-                                    payYear: 2022,
-                                    payMonth: 10, 
-                                    payDay: 25}}),
+                payedMoney: 20000000,
+                payment: "카드",
+                category: "식비",
+                memo: "안녕하세요",
+                payYear: 2022,
+                payMonth: 10,
+                payDay: 25
+            } }),
     });
-
-    const data = await response.json();
+    const data = yield response.json();
     console.log(data);
-}
+});
